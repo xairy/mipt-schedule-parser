8d780d74528fe9d8bfdc10db30a13299be8c77ec
Andrey Konovalov <adech.fo@gmail.com>
2013-10-18 23:16:31 +0400

    Schedule class

--- parser/parser.py
+++ parser/parser.py
@@ -16,83 +16,91 @@ SATURDAY = u'Суббота'
 DAYS = u'Дни'
 HOURS = u'Часы'
 
-def weekday_range(worksheet, weekday):
-  ranges = []
-  for (rb, re, cb, ce) in worksheet.merged_cells:
-    if worksheet.cell_value(rb, cb) == weekday:
-      ranges.append((rb, re))
-  assert len(ranges) != 0
-  for i in xrange(len(ranges) - 1):
-    assert ranges[i] == ranges[i + 1]
-  return ranges[0]
+class Schedule:
+  def __init__(self):
+    pass
 
-def department_count(worksheet):
-  days = 0
-  hours = 0
-  for row in xrange(worksheet.nrows):
-    for col in xrange(worksheet.ncols):
-      if worksheet.cell_value(row, col) == DAYS:
-        days += 1
-      if worksheet.cell_value(row, col) == HOURS:
-        hours += 1
-  assert days == hours
-  return days
+  def Parse(self, file):
+    self.workbook = xlrd.open_workbook(file, formatting_info=True)
+    self.worksheet = self.workbook.sheet_by_index(0)
 
-def department_range(worksheet, index):
-  days = []
-  hours = []
-  for row in xrange(worksheet.nrows):
-    for col in xrange(worksheet.ncols):
-      if worksheet.cell_value(row, col) == DAYS:
-        days.append(col)
-      if worksheet.cell_value(row, col) == HOURS:
-        hours.append(col)
-  days.sort()
-  hours.sort()
-  assert len(days) == len(hours)
-  assert index < len(days)
-  beg = hours[index] + 1
-  end = days[index + 1] if (index + 1 < len(days)) else worksheet.ncols
-  return (beg, end)
+    self.ParseWeekdays()
+    self.ParseDepartments()
+    self.ParseGroups()
 
-def departments_row(worksheet):
-  rows = []
-  for row in xrange(worksheet.nrows):
-    for col in xrange(worksheet.ncols):
-      if worksheet.cell_value(row, col) == DAYS:
-        rows.append(row)
-      if worksheet.cell_value(row, col) == HOURS:
-        rows.append(row)
-  rows.sort()
-  assert len(rows) > 0
-  for i in xrange(len(rows) - 1):
-    assert rows[i] == rows[i + 1]
-  return rows[0]
+  def ParseWeekdays(self):
+    self.weekday_ranges = []
+    weekdays = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY]
+    for day in weekdays:
+      ranges = []
+      for (rb, re, cb, ce) in self.worksheet.merged_cells:
+        if self.worksheet.cell_value(rb, cb) == day:
+          ranges.append((rb, re))
+      assert len(ranges) > 0
+      for i in xrange(len(ranges) - 1):
+        assert ranges[i] == ranges[i + 1]
+      self.weekday_ranges.append(ranges[0])
 
-def group_list(worksheet, department_index):
-  dep_range = department_range(worksheet, department_index)
-  dep_row = departments_row(worksheet)
-  groups = []
-  for column in xrange(dep_range[0], dep_range[1]):
-    group = worksheet.cell_value(dep_row, column).encode('ascii')
-    if group != '':
-      groups.append(group)
-  return groups
+  def ParseDepartments(self):
+    self.department_ranges = []
+    days_cols = []
+    hours_cols = []
+    rows = []
+    for row in xrange(self.worksheet.nrows):
+      for col in xrange(self.worksheet.ncols):
+        if self.worksheet.cell_value(row, col) == DAYS:
+          days_cols.append(col)
+          rows.append(row)
+        if self.worksheet.cell_value(row, col) == HOURS:
+          hours_cols.append(col)
+          rows.append(row)
+    assert len(days_cols) == len(hours_cols)
+    for k in xrange(len(days_cols)):
+      beg = hours_cols[k] + 1
+      end = days_cols[k + 1] if (k + 1 < len(days_cols)) else self.worksheet.ncols
+      self.department_ranges.append((beg, end))
+    assert len(rows) > 0
+    for k in xrange(len(rows) - 1):
+      assert rows[k] == rows[k + 1]
+    self.departments_row = rows[0]
 
-def group_count(worksheet, department_index):
-  return len(group_list(worksheet, department_index))
+  def ParseGroups(self):
+    self.groups = []
+    self.group_ranges = []
+    for department in xrange(self.GetDepartmentCount()):
+      dep_groups = []
+      dep_group_ranges = []
+      dep_range = self.department_ranges[department]
+      dep_row = self.departments_row
+      for column in xrange(dep_range[0], dep_range[1]):
+        beg, end = 0, 0
+        group = self.worksheet.cell_value(dep_row, column).encode('ascii')
+        if group != '':
+          dep_groups.append(group)
+          beg, end = column, column + 1
+          while end < self.worksheet.ncols and len(self.worksheet.cell_value(dep_row, end)) == 0:
+            end += 1
+          dep_group_ranges.append((beg, end))
+      self.groups.append(dep_groups)
+      self.group_ranges.append(dep_group_ranges)
 
-def group_range(worksheet, department_index, group_index):
-  dep_range = department_range(worksheet, department_index)
-  dep_row = departments_row(worksheet)
-  groups = group_list(worksheet, department_index)
-  group = groups[group_index]
-  beg, end = 0, 0
-  for column in xrange(dep_range[0], dep_range[1]):
-    if worksheet.cell_value(dep_row, column).encode('ascii') == group:
-      beg = column
-  end = beg + 1
-  while end < worksheet.ncols and len(worksheet.cell_value(dep_row, end)) == 0:
-    end += 1
-  return (beg, end)
+  def GetWeekdayRange(self, weekday_index):
+    return self.weekday_ranges[weekday_index]
 
+  def GetDepartmentCount(self):
+    return len(self.department_ranges)
+
+  def GetDepartmentRange(self, department_index):
+    return self.department_ranges[department_index]
+
+  def GetDepartmentsRow(self):
+    return self.departments_row
+
+  def GetGroupCount(self, department_index):
+    return len(self.groups[department_index])
+
+  def GetGroupList(self, department_index):
+    return self.groups[department_index]
+
+  def GetGroupRange(self, department_index, group_index):
+    return self.group_ranges[department_index][group_index]
--- parser/tests.py
+++ parser/tests.py
@@ -8,83 +8,91 @@ import parser
 
 class WeekdayRangeTest(unittest.TestCase):
   def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
 
   def runTest(self):
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.MONDAY), (4, 11))
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.TUESDAY), (12, 19))
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.WEDNESDAY), (20, 27))
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.THURSDAY), (28, 37))
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.FRIDAY), (38, 47))
-    self.assertEqual(parser.weekday_range(self.worksheet, parser.SATURDAY), (48, 57))
+    self.assertEqual(self.schedule.GetWeekdayRange(0), (4, 11))
+    self.assertEqual(self.schedule.GetWeekdayRange(1), (12, 19))
+    self.assertEqual(self.schedule.GetWeekdayRange(2), (20, 27))
+    self.assertEqual(self.schedule.GetWeekdayRange(3), (28, 37))
+    self.assertEqual(self.schedule.GetWeekdayRange(4), (38, 47))
+    self.assertEqual(self.schedule.GetWeekdayRange(5), (48, 57))
+
+class DepartmentCountTest(unittest.TestCase):
+  def setUp(self):
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
+
+  def runTest(self):
+    self.assertEqual(self.schedule.GetDepartmentCount(), 9)
 
 class DepartmentRangeTest(unittest.TestCase):
   def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
 
   def runTest(self):
-    self.assertEqual(parser.department_count(self.worksheet), 9)
-    self.assertEqual(parser.department_range(self.worksheet, 0), (2, 11))
-    self.assertEqual(parser.department_range(self.worksheet, 1), (13, 20))
-    self.assertEqual(parser.department_range(self.worksheet, 2), (22, 32))
-    self.assertEqual(parser.department_range(self.worksheet, 3), (34, 36))
-    self.assertEqual(parser.department_range(self.worksheet, 4), (38, 43))
-    self.assertEqual(parser.department_range(self.worksheet, 5), (45, 53))
-    self.assertEqual(parser.department_range(self.worksheet, 6), (55, 62))
-    self.assertEqual(parser.department_range(self.worksheet, 7), (64, 71))
-    self.assertEqual(parser.department_range(self.worksheet, 8), (73, 77))
+    self.assertEqual(self.schedule.GetDepartmentRange(0), (2, 11))
+    self.assertEqual(self.schedule.GetDepartmentRange(1), (13, 20))
+    self.assertEqual(self.schedule.GetDepartmentRange(2), (22, 32))
+    self.assertEqual(self.schedule.GetDepartmentRange(3), (34, 36))
+    self.assertEqual(self.schedule.GetDepartmentRange(4), (38, 43))
+    self.assertEqual(self.schedule.GetDepartmentRange(5), (45, 53))
+    self.assertEqual(self.schedule.GetDepartmentRange(6), (55, 62))
+    self.assertEqual(self.schedule.GetDepartmentRange(7), (64, 71))
+    self.assertEqual(self.schedule.GetDepartmentRange(8), (73, 77))
 
 class DepartmentsRowTest(unittest.TestCase):
   def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
 
   def runTest(self):
-    self.assertEqual(parser.departments_row(self.worksheet), 3)
+    self.assertEqual(self.schedule.GetDepartmentsRow(), 3)
 
 class GroupCountTest(unittest.TestCase):
   def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
 
   def runTest(self):
-    self.assertEqual(parser.group_count(self.worksheet, 0), 9)
-    self.assertEqual(parser.group_count(self.worksheet, 1), 7)
-    self.assertEqual(parser.group_count(self.worksheet, 2), 8)
-    self.assertEqual(parser.group_count(self.worksheet, 3), 2)
-    self.assertEqual(parser.group_count(self.worksheet, 4), 5)
-    self.assertEqual(parser.group_count(self.worksheet, 5), 8)
-    self.assertEqual(parser.group_count(self.worksheet, 6), 7)
-    self.assertEqual(parser.group_count(self.worksheet, 7), 7)
-    self.assertEqual(parser.group_count(self.worksheet, 8), 4)
-
-class GroupRangeTest(unittest.TestCase):
-  def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
-
-  def runTest(self):
-    self.assertEqual(parser.group_range(self.worksheet, 0, 0), (2, 3))
-    self.assertEqual(parser.group_range(self.worksheet, 0, 1), (3, 4))
-    self.assertEqual(parser.group_range(self.worksheet, 2, 1), (23, 25))
-    self.assertEqual(parser.group_range(self.worksheet, 2, 2), (25, 26))
-    self.assertEqual(parser.group_range(self.worksheet, 2, 3), (26, 28))
-    self.assertEqual(parser.group_range(self.worksheet, 5, 3), (48, 49))
-    self.assertEqual(parser.group_range(self.worksheet, 8, 0), (73, 74))
-    self.assertEqual(parser.group_range(self.worksheet, 8, 3), (76, 77))
+    self.assertEqual(self.schedule.GetGroupCount(0), 9)
+    self.assertEqual(self.schedule.GetGroupCount(1), 7)
+    self.assertEqual(self.schedule.GetGroupCount(2), 8)
+    self.assertEqual(self.schedule.GetGroupCount(3), 2)
+    self.assertEqual(self.schedule.GetGroupCount(4), 5)
+    self.assertEqual(self.schedule.GetGroupCount(5), 8)
+    self.assertEqual(self.schedule.GetGroupCount(6), 7)
+    self.assertEqual(self.schedule.GetGroupCount(7), 7)
+    self.assertEqual(self.schedule.GetGroupCount(8), 4)
 
 class GroupListTest(unittest.TestCase):
   def setUp(self):
-    self.workbook = xlrd.open_workbook('2013_fall/4kurs.xls', formatting_info=True)
-    self.worksheet = self.workbook.sheet_by_index(0)
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
 
   def runTest(self):
-    self.assertEqual(parser.group_list(self.worksheet, 0), ['011', '012', '013', '014', '015', '016', '017', '018', '019'])
-    self.assertEqual(parser.group_list(self.worksheet, 1), ['021', '022', '023', '024', '025', '026', '028'])
-    self.assertEqual(parser.group_list(self.worksheet, 3), ['041', '042'])
-    self.assertEqual(parser.group_list(self.worksheet, 8), ['0111', '0112', '0113', '0114'])
+    self.assertEqual(self.schedule.GetGroupList(0), ['011', '012', '013', '014', '015', '016', '017', '018', '019'])
+    self.assertEqual(self.schedule.GetGroupList(1), ['021', '022', '023', '024', '025', '026', '028'])
+    self.assertEqual(self.schedule.GetGroupList(3), ['041', '042'])
+    self.assertEqual(self.schedule.GetGroupList(8), ['0111', '0112', '0113', '0114'])
+
+
+class GroupRangeTest(unittest.TestCase):
+  def setUp(self):
+    self.schedule = parser.Schedule()
+    self.schedule.Parse('2013_fall/4kurs.xls')
+
+  def runTest(self):
+    self.assertEqual(self.schedule.GetGroupRange(0, 0), (2, 3))
+    self.assertEqual(self.schedule.GetGroupRange(0, 1), (3, 4))
+    self.assertEqual(self.schedule.GetGroupRange(2, 1), (23, 25))
+    self.assertEqual(self.schedule.GetGroupRange(2, 2), (25, 26))
+    self.assertEqual(self.schedule.GetGroupRange(2, 3), (26, 28))
+    self.assertEqual(self.schedule.GetGroupRange(5, 3), (48, 49))
+    self.assertEqual(self.schedule.GetGroupRange(8, 0), (73, 74))
+    self.assertEqual(self.schedule.GetGroupRange(8, 3), (76, 77))
 
 if __name__ == '__main__':
    unittest.main()
