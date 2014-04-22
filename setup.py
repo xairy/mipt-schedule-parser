669bcbaed0e5c94878d662da66d42c666a64e4cc
Andrey Konovalov <adech.fo@gmail.com>
2014-04-22 17:52:32 +0400

    Fixed package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name='MIPT Schedule Parser',
+setup(name='mipt-schedule-parser',
     version='0.1.0',
     description='Parses standard mipt schedule xls file.',
 
